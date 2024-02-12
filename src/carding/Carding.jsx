e28b5d9b97206571c37f80dacb52a56f0c0427be
Abhiraj412 <abhangparth@gmail.com>
2024-02-13 03:24:19 +0530

    commit1

--- src/carding/Carding.jsx
+++ src/carding/Carding.jsx
@@ -11,25 +11,25 @@ function Cardimg() {
     <Container className='pt-5'>
       <Row>
         <Col xs={12} md={6}>
-          <Card className="custom-card" style={{padding: "20px", margin: "20px"}}>
+          <Card className="custom-card" style={{ padding: "20px", margin: "20px" }}>
             <Card.Body>
               <Card.Title><i className="bi bi-people-fill text-primary icon1"></i> People</Card.Title>
               <Card.Text>
                 Find a teacher, coach, or expert for your hobby interest in your locality. Find a partner, teammate, accompanist, or collaborator.
               </Card.Text>
-              <Button className="border-primary text-primary cardbtn1">Connect</Button>
+              <Button className="btn btn-light " style={{ color: '#8064A2', borderColor: '#8064A2' }}>Connect</Button>
             </Card.Body>
           </Card>
         </Col>
 
         <Col xs={12} md={6}>
-          <Card className="custom-card" style={{padding: "20px", margin: "20px"}}>
+          <Card className="custom-card" style={{ padding: "20px", margin: "20px" }}>
             <Card.Body >
               <Card.Title><i className="bi bi-geo-alt-fill text-success icon2"></i> Place</Card.Title>
               <Card.Text>
                 Find a class, school, playground, auditorium, studio, shop, or an event venue. Book a slot at venues that allow booking through hobbycue.
               </Card.Text>
-              <Button className="border-primary text-primary cardbtn2">Meet up</Button>
+              <Button className="btn btn-light " style={{ color: '#8064A2', borderColor: '#8064A2' }}>Meet up</Button>
             </Card.Body>
           </Card>
         </Col>
@@ -37,25 +37,25 @@ function Cardimg() {
 
       <Row>
         <Col xs={12} md={6}>
-          <Card className="custom-card" style={{padding: "20px", margin: "20px"}}>
+          <Card className="custom-card" style={{ padding: "20px", margin: "20px" }}>
             <Card.Body>
               <Card.Title><i className="bi bi-basket3-fill text-danger icon3"></i> Product</Card.Title>
               <Card.Text>
                 Find equipment or supplies required for your hobby. Buy, rent, or borrow from shops, online stores, or community members.
               </Card.Text>
-              <Button className="border-primary text-primary cardbtn3">Get it</Button>
+              <Button className="btn btn-light " style={{ color: '#8064A2', borderColor: '#8064A2' }}>Get it</Button>
             </Card.Body>
           </Card>
         </Col>
 
         <Col xs={12} md={6}>
-          <Card className="custom-card" style={{padding: "20px", margin: "20px"}}>
+          <Card className="custom-card" style={{ padding: "20px", margin: "20px" }}>
             <Card.Body>
               <Card.Title><i className="bi bi-calendar-check-fill text-info icon4"></i> Program</Card.Title>
               <Card.Text>
                 Find events, meetups, and workshops related to your hobby. Register or buy tickets online.
               </Card.Text>
-              <Button className="border-primary text-primary cardbtn4">Attend</Button>
+              <Button className="btn btn-light " style={{ color: '#8064A2', borderColor: '#8064A2' }}>Attend</Button>
             </Card.Body>
           </Card>
         </Col>
